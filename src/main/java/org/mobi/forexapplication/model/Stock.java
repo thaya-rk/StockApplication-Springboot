22e41330e49bbd8ba3ad0fbb777efbd2136174b4
Thayanantham Kaliappan <thayanantham.k@mobiasia.com>
2025-05-17 08:57:36 +0530

    validation changes

--- src/main/java/org/mobi/forexapplication/model/Stock.java
+++ src/main/java/org/mobi/forexapplication/model/Stock.java
@@ -3,10 +3,7 @@ package org.mobi.forexapplication.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
--- src/main/java/org/mobi/forexapplication/serviceImpl/PortfolioServiceImpl.java
+++ src/main/java/org/mobi/forexapplication/serviceImpl/PortfolioServiceImpl.java
@@ -19,7 +19,6 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PortfolioServiceImpl implements PortfolioService {
